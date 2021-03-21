ac62777f30fd2261ebcb28a402a7e6d719184895
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-03-21 16:00:18 +0200

    added loops

--- HTML/app.js
+++ HTML/app.js
@@ -5,8 +5,12 @@ var WelcomeMessage="You are welcome to Solidworks Jordan";
 
 alert(WelcomeMessage);
 
+var ConfirmMessage = ""
+while (ConfirmMessage !== "yes"){
 
-var ConfirmMessage=confirm("would you like to know who we are ?")
+  ConfirmMessage =prompt("would you like to know who we are ?")
+
+}
 
 if(ConfirmMessage==true)
 {document.write('<h3>'+'We are a specialet page in the mechanical design field, we can help you to convert your ideas into real things, we put the years of experience on your hand.'+'</h3>')}
@@ -14,14 +18,49 @@ else{document.write('<h3>'+'IF you are not interested, THANK YOU for your visit!
 
 console.log(ConfirmMessage);
 
-var OurService=prompt("Which is the service you search about ? 3D Modeling || 3D Rendering || 3D Design ?")
+ 
 
-if(OurService=='3D Modeling')
-   {document.write('<img src="pictures/3D printer.JPG" style="width:550px"/>')}
+var OurService = ""
+var numberOfServices=0
+var resultServices=""
+    for(i=0;(OurService!== ("3D Modeling"||"3D Rendering"||"3D Design"))&& !(numberOfServices >= 1 && numberOfServices <= 5); i++)
+      
+{
 
-    else if(OurService=='3D Rendering')
-             {document.write('<img src="pictures/RO plant.JPG" style="width:550px"/>')}
+  OurService=prompt("Which is the service you search about ? 3D Modeling || 3D Rendering || 3D Design ?")
 
-           else if(OurService=='3D Design')
-             {document.write('<img src="pictures/wood2.jpg" style="width:550px"/>')}
-               else{}
+  numberOfServices=prompt("How much the number of services tha's you need ?")
+
+       if((OurService!== ("3D Modeling"||"3D Rendering"||"3D Design"))&& !(numberOfServices >= 1 && numberOfServices <= 5)){
+             alert("please select one of main three our services : 3D Modelind,3D Rendering, 3D Design and please input a number of 1 - 5 services")}
+                 else{alert("Thank You, Great!")}
+ 
+
+                 
+                 
+                 
+                }
+                if(OurService=='3D Modeling'){
+                   resultServices='<img src="pictures/3D printer.JPG" style="width:550px"/></br>'}
+                
+                    else if(OurService=='3D Rendering')
+                             {resultServices='<img src="pictures/RO plant.JPG" style="width:550px"/></br>'}
+                
+                           else if(OurService=='3D Design')
+                             {resultServices='<img src="pictures/wood2.jpg" style="width:550px"/></br>'}
+                               else{}
+  
+    // var x =0 
+    // var y = x+1
+    var finalResults ='' 
+    var numbering = 1
+    console.log("numberOfServices",numberOfServices);                         
+    for(x=0;x<numberOfServices;x++){
+
+      finalResults+=(numbering+x)+resultServices;
+
+
+    }
+    console.log('finalResults',finalResults)
+
+    document.write(finalResults);
