d8f07efe71b403a55f737a540f1143c413515739
Ahmad Abu Osbeh <ahmadabuosbeh20@gmail.com>
2021-03-23 04:00:09 +0200

    added functions to JS code

--- HTML/app.js
+++ HTML/app.js
@@ -25,26 +25,35 @@ console.log(ConfirmMessage);
 
  
 
-var OurService = ""
-var numberOfServices=0
+var OurService  =prompt("Which is the service you search about ? 3D Modeling || 3D Rendering || 3D Design ?")
+var numberOfServices=prompt("How much the number of services tha's you need ?")
 var resultServices=""
-    for(i=0;(OurService!== ("3D Modeling"||"3D Rendering"||"3D Design"))&& !(numberOfServices >= 1 && numberOfServices <= 5); i++)
-      
-{
 
-  OurService=prompt("Which is the service you search about ? 3D Modeling || 3D Rendering || 3D Design ?")
+function getUserService(userService,numberOFit){
+  
+  for(i=0;(userService!== ("3D Modeling"||"3D Rendering"||"3D Design"))&& !(numberOFit >= 1 && numberOFit <= 5); i++)
+  
+  {
+    
+    userService=prompt("Which is the service you search about ? 3D Modeling || 3D Rendering || 3D Design ?")
+    
+    numberOFit=prompt("How much the number of services tha's you need ?")
+    
+    
+    
+  }
+  // return userService,numberOFit
+  
+}
+getUserService(OurService,numberOfServices);
+console.log("OurService",OurService,"numberOfServices",numberOfServices);
+if((OurService!== ("3D Modeling"||"3D Rendering"||"3D Design"))&& !(numberOfServices >= 1 && numberOfServices <= 5)){
+  alert("please select one of main three our services : 3D Modelind,3D Rendering, 3D Design and please input a number of 1 - 5 services")}
+  else{alert("Thank You, Great!")}
+              // document.write()
 
-  numberOfServices=prompt("How much the number of services tha's you need ?")
 
-       if((OurService!== ("3D Modeling"||"3D Rendering"||"3D Design"))&& !(numberOfServices >= 1 && numberOfServices <= 5)){
-             alert("please select one of main three our services : 3D Modelind,3D Rendering, 3D Design and please input a number of 1 - 5 services")}
-                 else{alert("Thank You, Great!")}
- 
 
-                 
-                 
-                 
-                }
                 if(OurService=='3D Modeling'){
                    resultServices='<img src="pictures/3D printer.JPG" style="width:550px"/></br>'}
                 
@@ -55,20 +64,25 @@ var resultServices=""
                              {resultServices='<img src="pictures/wood2.jpg" style="width:550px"/></br>'}
                                else{}
   
-    // var x =0 
+    var x =0 
     // var y = x+1
     var finalResults ='' 
     var numbering = 1
-    console.log("numberOfServices",numberOfServices);                         
-    for(x=0;x<numberOfServices;x++){
+    // console.log("numberOfServices",numberOfServices);  
+     function reultsByFunction(userService2,numberOFit2){                     
+    for(x=0;x<numberOFit2;x++){
 
-      finalResults+=(numbering+x)+resultServices;
-
+      finalResults+=(numbering+x)+userService2;
+      
+      // console.log('finalResults',finalResults)
 
     }
-    console.log('finalResults',finalResults)
-
-    document.write(finalResults);
+    return finalResults
+  }
+    reultsByFunction(resultServices,numberOfServices)
+    console.log("finalResults",finalResults);
+    // console.log("finalResults",finalResults);
+    // document.write(reultsByFunction());
 
 //     var i = l;
 // var msg = '';
